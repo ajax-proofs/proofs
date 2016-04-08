668c24528b281e7f314a7b7c00e12422a893ce18
K Stanton <KMStanton@users.noreply.github.com>
2016-04-08 15:24:41 -0500

    Update proof.js

--- proof.js
+++ proof.js
@@ -1,3 +1,16 @@
+function swapContent(href, url_data, target) {
+    $.ajax({
+        type: 'GET',
+        cache: false,
+        url: href+'?' + url_data,  //add a variable to the URL that will carry the value in your i counter through to the PHP page so it know's if this is new or additional data
+        success: function (data) { // this param name was confusing, I have changed it to the "normal" name to make it clear that it contains the data returned from the request
+            //load more data to "target" value div
+            target.innerHTML = (data); // as above, data holds the result of the request, so all the data returned from your results.php file are in this param but please see below
+        }
+    })
+
+}
+
 $(function() {
     $( "#accordion" ).accordion();
 });
@@ -10,3 +23,34 @@ function live_search(){
     insert = document.getElementById('insert').value;
     swapContent("existence_hell/server_vars.php", "insert="+insert , content);
 }
+
+
+$(function() {
+    var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+        source: availableTags
+    });
+});
